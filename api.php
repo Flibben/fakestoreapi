2fb9b90440f05c94a5ff6aa6c518d888b3302158
Flibben <flibben@gmail.com>
2021-05-07 13:27:20 +0200

    assignment 2 for backend2 php

---
+++ api.php
@@ -0,0 +1,66 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Referrer-Policy: no-referrer");
+
+include_once 'products.php';
+
+$dataArray = [];
+$errorArray = [];
+
+if (!isset($_GET['category'])) {
+	$dataArray = $products;
+} elseif (isset($_GET['category'])) {
+	$category = $_GET['category'];
+
+	switch ($category) {
+		case "mens clothing":
+			for ($i = 0; $i <= count($products) - 1; $i++) {
+				if ($products[$i]['category'] === "mens clothing") {
+					array_push($dataArray, $products[$i]);
+				}
+			}
+			break;
+		case "womens clothing":
+			for ($i = 0; $i <= count($products) - 1; $i++) {
+				if ($products[$i]['category'] == "womens clothing") {
+					array_push($dataArray, $products[$i]);
+				}
+			}
+			break;
+		case "electronics":
+			for ($i = 0; $i <= count($products) - 1; $i++) {
+				if ($products[$i]['category'] == "electronics") {
+					array_push($dataArray, $products[$i]);
+				}
+			}
+			break;
+		default:
+			array_push($errorArray, ["Category" => $_GET['category'] . " is not a valid category"]);
+	}
+}
+
+//Counts after category is chosen
+$count = count($dataArray);
+
+if (!isset($_GET['show'])) {
+	//do nothing
+} elseif ($_GET['show'] > $count || empty($_GET['show']) && $_GET['show'] != "0") {
+	array_push($errorArray, [
+		"show" => "invalid input"
+	]);
+} elseif (isset($_GET['show'])) {
+	$show = $_GET['show'];
+	shuffle($dataArray);
+	$dataArray = array_slice($dataArray, 0, $show);
+}
+
+if (count($errorArray)) {
+	echo json_encode($errorArray, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+} else {
+	echo json_encode($dataArray, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+}
---
+++ products.php
@@ -0,0 +1,169 @@
+<?php
+$products = [
+    [
+        "id" => 1,
+        "product name " => "Deluxe Camera",
+        "product description" => "One camera for all your needs",
+        "price" => 69.99,
+        "img" => "https://cdn.mos.cms.futurecdn.net/eKCXUTEEjqFtpByKXU6zS3-970-80.jpg",
+        "category" => "electronics"
+    ],
+    [
+        "id" => 2,
+        "product name" => "Khaki Shorts",
+        "product description" => "The shorts that you dreamt of last night to blend into the Sarah sand.",
+        "price" => 43.34,
+        "img" => "https://images.tailorstore.com/YToyOntzOjU6IndpZHRoIjtpOjExNTI7czo2OiJoZWlnaHQiO2I6MDt9/images/catalog/Tailor-Store-Wales-beige-shorts-01-1.jpg",
+        "category" => "mens clothing"
+    ],
+    [
+        "id" => 3,
+        "product name" => "Pleasant skirt",
+        "product description" => "The skirt that's so pleasant that you want to where it all the time",
+        "price" => 23.23,
+        "img" => "https://image.s5a.com/is/image/saks/0400013397026_BEIGE",
+        "category" => "womens clothing"
+    ],
+    [
+        "id" => 4,
+        "product name" => "Business Shirt",
+        "product description" => "With this shirt on your back you'll empower both your team and yourself",
+        "price" => 42.0,
+        "img" => "https://ehshirts.com/wp-content/uploads/2020/02/white-shirt-with-suit.jpg",
+        "category" => "womens clothing"
+    ],
+    [
+        "id" => 5,
+        "product name" => "Fundamental rights t-shirt",
+        "product description" => "For the person who want fundamental rights",
+        "price" => 3.69,
+        "img" => "https://truecolorsunited.org/wp-content/uploads/2017/03/cyndi-girls-t-shirt-new.jpg",
+        "category" => "womens clothing"
+    ],
+    [
+        "id" => 6,
+        "product name" => "Wanted Hoodie",
+        "product description" => "Well behaved women rarely make history, you know what to do",
+        "price" => 9.99,
+        "img" => "https://i.pinimg.com/originals/08/6a/8a/086a8a57c8206d4853576ed8f5e82f31.jpg",
+        "category" => "womens clothing"
+    ],
+    [
+        "id" => 7,
+        "product name" => "Hifiman HE 6",
+        "product description" => "For the humans who treats their ears better than their offsprings",
+        "price" => 1249,
+        "img" => "https://financesonline.com/uploads/hifiman.jpg",
+        "category" => "electronics"
+    ],
+    [
+        "id" => 8,
+        "product name" => "Mankini",
+        "product description" => "For the stylish man at the beach",
+        "price" => 3.69,
+        "img" => "https://assets.partyking.org/img/products/1300/mankini-rosa-1.jpg",
+        "category" => "mens clothing"
+    ],
+    [
+        "id" => 9,
+        "product name" => "The man purse",
+        "product description" => "For the modern man that doesnt have pockets",
+        "price" => 456.42,
+        "img" => "https://media.gq-magazine.co.uk/photos/5d1397009fa601bd0c8386a6/master/w_1024%2cc_limit/The-Hangover-12July2016_rex_b.jpg",
+        "category" => "mens clothing"
+    ],
+    [
+        "id" => 10,
+        "product name" =>  "Leatherbound Filofax",
+        "product description" => "For the person who wants to keep it simple, analog style is never out of style",
+        "price" => 321.34,
+        "img" => "https://i.etsystatic.com/22585061/r/il/4b0a15/2284604303/il_1140xN.2284604303_qf1k.jpg",
+        "category" => "mens clothing"
+    ],
+    [
+        "id" => 11,
+        "product name" => "Leather Jacket",
+        "product description" => "For the MC-ride or just your casual workplace AW hang",
+        "price" => 112.98,
+        "img" => "https://sc04.alicdn.com/kf/Ub751b92bca414aea8c3431fc0eb4b39fv.jpg",
+        "category" => "womens clothing"
+    ],
+    [
+        "id" => 12,
+        "product name" => "Wolf Shirt",
+        "product description" => "You know that you're the alfa around here, right?",
+        "price" => 43.57,
+        "img" => "https://m.media-amazon.com/images/I/71sgoFH7kbL._AC_SX569._SX._UX._SY._UY_.jpg",
+        "category" => "mens clothing"
+    ], [
+        "id" => 13,
+        "product name" => "Badass business t-shirt",
+        "product description" => "For the woman who knows that she's great at what she does",
+        "price" => 9.99,
+        "img" => "https://cdn.shopify.com/s/files/1/0403/5638/0822/products/tattooed-girl-mockup-standing-while-wearing-a-round-neck-tee-against-a-solid-color-background-a17178.png?v=1611441870",
+        "category" => "womens clothing",
+    ],
+    [
+        "id" => 14,
+        "product name" => "Hausbell Flashlight Two-Pack",
+        "product description" => "Hausbell's twin-pair of miniature flashlights are ideal for tool kits and outdoor excursions",
+        "price" => 99.99,
+        "img" => "https://cdn.pocket-lint.com/r/s/660x/assets/images/154076-gadgets-news-buyer-s-guide-best-mini-flashlights-image2-qjdsgbf8yp-jpg.webp?v1",
+        "category" => "electronics",
+
+    ],
+    [
+        "id" => 15,
+        "product name" => "Acer Swift 3",
+        "product description" => "When you want the best bang for your buck, the Acer Swift 3 proves that you can get great performance at a surprisingly low price",
+        "price" => 700,
+        "img" => "https://cdn.mos.cms.futurecdn.net/iYCQTPgBSdDmkYESfPkunh-970-80.jpg.webp",
+        "category" => "electronics",
+
+    ],
+    [
+        "id" => 16,
+        "product name" => "SARS-CoV-2",
+        "product description" => "Your next business meeting could be inside a VR headset",
+        "price" => 1231,
+        "img" => "https://static.dw.com/image/48183155_401.jpg",
+        "category" => "electronics",
+
+    ],
+    [
+        "id" => 17,
+        "product name" => "Slayer 34",
+        "product description" => "For the party where you does not really want to stand",
+        "price" => 169.42,
+        "img" => "https://i.pinimg.com/236x/5d/f6/66/5df666744167418e59164538eb0dc4ec.jpg",
+        "category" => "womens clothing",
+
+    ],
+    [
+        "id" => 18,
+        "product name" => "Goldie",
+        "product description" => "These shoes will make you shine in all the right ways",
+        "price" => 269.42,
+        "img" => "https://chekich.com/i/l/000/0001726_chekich-ch259-bt-black-gold-mens-sneakers.jpeg",
+        "category" => "mens clothing",
+
+    ],
+    [
+        "id" => 19,
+        "product name" => "Elegant Watch",
+        "product description" => "You look great and you can also tell what time it is, the best of two worlds",
+        "price" => 999.99,
+        "img" => "https://images-na.ssl-images-amazon.com/images/I/71pXdd3S5xL._AC_UX522_.jpg",
+        "category" => "electronics",
+
+    ],
+    [
+        "id" => 20,
+        "product name" => "Contactless Thermometer",
+        "product description" => "Temperature without touching, perfect during a pandemic",
+        "price" => 99.99,
+        "img" => "https://cdn.alzashop.com/ImgW.ashx?fd=f16&cd=LAIBT668",
+        "category" => "electronics",
+
+    ]
+];
